16226cae2168fab3b349c2b8a3b5ffa54dd41032
Kyanari <kyanari111@gmail.com>
2023-02-14 15:50:05 -0800

    Created join command.

--- commands/profile.js
+++ commands/profile.js
@@ -12,35 +12,35 @@ module.exports = {
             .setRequired(false)),
             
 	async execute(interaction) {
-        const getPlayer = interaction.options.getMember('player');
+        const getPlayer = interaction.options.getUser('player');
         const member = getPlayer === null ? interaction.user : getPlayer;
+        const numFormat = (value) => new Intl.NumberFormat('en-US').format(value === null ? 0 : value);
 
         const guild = interaction.guild;
         try {
             const player = await Player.findOne({ where: { discordID: member.id, guildID: guild.id }, include: 'iura' });
-
             const embed = new EmbedBuilder()
                 .setColor(0xFF0000)
                 .setTitle('**ADVENTURER ID CARD**')
                 .setAuthor({ name: `${member.tag}` })
                 .setThumbnail(`${member.displayAvatarURL({ extension: 'png', size: 512 })}`)
                 .addFields(
-                    { name: 'Player Name', value: `${player.playerName}`, inline: false },
-                    { name: 'Faction', value: `${player.faction}`, inline: false },
-                    { name: 'Max Health', value: `${player.totalHealth}`, inline: false },
-                    { name: 'Attack', value: `${player.totalAttack}`, inline: true },
-                    { name: 'Defense', value: `${player.totalDefense}`, inline: true },
-                    { name: 'Weapon', value: `${player.weapon}`, inline: false },
-                    { name: 'Armor', value: `${player.armor}`, inline: false },
-                    { name: 'Iura', value: `${player.iura.walletAmount}`, inline: false },
+                    { name: '👤 Player Name', value: `${player.playerName}`, inline: false },
+                    { name: '👥 Faction', value: `${player.faction}`, inline: false },
+                    { name: '🩸 Health', value: `${player.totalHealth}`, inline: false },
+                    { name: '⚔️ Attack', value: `${player.totalAttack}`, inline: true },
+                    { name: '🛡️ Defense', value: `${player.totalDefense}`, inline: true },
+                    { name: '🗡️ Weapon', value: `${player.weapon}`, inline: false },
+                    { name: '💠 Armor', value: `${player.armor}`, inline: false },
+                    { name: '💰 Iura', value: `$${numFormat(player.iura.walletAmount)}`, inline: false },
                 )
+                .setImage(`${player.imageURL}`)
                 .setFooter({ text: 'This bot was made by megura.xyz.' });
 
-            await interaction.reply({ embeds: [embed] })
-                .catch(console.error);
+            await interaction.reply({ embeds: [embed] });
         } catch (error) {
             interaction.reply("This user does not have a player profile in this world yet.");
-            console.log(console.error);
+            console.log(error);
         }
 	}
 };
