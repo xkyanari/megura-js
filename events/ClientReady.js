19ac178c826242d84c5f487d4e5c6a4a41100179
Kyanari <kyanari111@gmail.com>
2023-02-27 10:28:45 -0800

    Patch 1.1.7 adjustments

--- events/ClientReady.js
+++ events/ClientReady.js
@@ -18,8 +18,15 @@ module.exports = {
 		client.user.setPresence({ activities: [{ name: 'Messinia Graciene', type: ActivityType.Playing }], status: 'dnd' });
 	console.log( `You're now connected as ${client.user.tag}.\nNode version: ${process.version}\nDiscord.js version: ${Discord.version}` );
 
-	sequelize.sync() // { alter/force: true }
-		.then(data => console.log("Database connection successful."))
+	sequelize.sync({ alter: true }) // { alter/force: true }
+		.then((data) => console.log("Database connection successful.", data))
+		// .then(() => {
+		// 	const quests = require('../assets/quest_db.json');
+		// 	for (let quest = 0; quest < quests.length; quest++) {
+		// 		Quest.create(quests[quest]);
+		// 	}
+		// 	console.log("Quest data import completed.")
+		// })
 		// .then(() => {
 		// 	const monsters = require('../assets/mob_db.json');
 		// 	for (let monster = 0; monster < monsters.length; monster++) {
@@ -34,62 +41,13 @@ module.exports = {
 		// 	}
 		// 	console.log("Item data import completed.")
 		// })
-		// // importing from the old db
 		// .then(() => {
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '119671611803697152', playerName: 'Kyanari', faction: 'Margaretha' });
+		// 	const contracts = require('../assets/contracts.json');
+		// 	for (let contract = 0; contract < contracts.length; contract++) {
+		// 		Contract.create(contracts[contract]);
+		// 	}
+		// 	console.log("Contract data import completed.")
 		// })
-		// .then((kyanari) => {
-		// 	return kyanari.createIura({ walletName: 'Kyanari', bankName: 'Kyanari', walletAmount: '10000000' });
-		// })
-		// .then(() => {
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '566767755915821056', playerName: 'boo', faction: 'Cerberon' });
-		// })
-		// .then((dataA) => {
-		// 	dataA.createIura({ walletName: 'boo', bankName: 'boo' });
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '919448412502519808', playerName: 'Julio Valiente', faction: 'Cerberon' });
-		// })
-		// .then((dataB) => {
-		// 	return dataB.createIura({ walletName: 'Julio Valiente', bankName: 'Julio Valiente' });
-		// })
-		// .then(() => {
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '612700763541733409', playerName: 'Hentai Lord', faction: 'Cerberon' });
-		// })
-		// .then((dataC) => {
-		// 	dataC.createIura({ walletName: 'Hentai Lord', bankName: 'Hentai Lord' });
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '240105227531845632', playerName: 'Lenexy', faction: 'Margaretha' });
-		// })
-		// .then((dataD) => {
-		// 	return dataD.createIura({ walletName: 'Lenexy', bankName: 'Lenexy' });
-		// })
-		// .then(() => {
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '395262443846107136', playerName: 'Raych', faction: 'Cerberon' });
-		// })
-		// .then((dataE) => {
-		// 	dataE.createIura({ walletName: 'Raych', bankName: 'Raych' });
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '282772521919447040', playerName: '177013', faction: 'Cerberon' });
-		// })
-		// .then((dataF) => {
-		// 	return dataF.createIura({ walletName: '177013', bankName: '177013' });
-		// })
-		// .then(() => {
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '930685195475099658', playerName: 'Lone Drifter', faction: 'Cerberon' });
-		// })
-		// .then((dataG) => {
-		// 	return dataG.createIura({ walletName: 'Lone Drifter', bankName: 'Lone Drifter' });
-		// })
-		// .then(() => {
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '248890983985577984', playerName: 'Altz', faction: 'Cerberon' });
-		// })
-		// .then((dataH) => {
-		// 	return dataH.createIura({ walletName: 'Altz', bankName: 'Altz'});
-		// })
-		// .then(() => {
-		// 	return Player.create({ guildID: '1032034043686035508', discordID: '111289101176565760', playerName: 'Nyasuri', faction: 'Cerberon' });
-		// })
-		// .then((dataI) => {
-		// 	return dataI.createIura({ walletName: 'Nyasuri', bankName: 'Nyasuri'});
-		// })
-		// .then(() => console.log("Profiles created successfully."))
 		.catch((error) => {
 			console.log("Error database connection.");
 			console.log(error);
