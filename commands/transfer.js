186dd867e98510e563d25c342c9ce7069e1328b7
Kyanari <kyanari111@gmail.com>
2023-02-28 09:01:17 -0800

    Added number format on transfer

--- commands/transfer.js
+++ commands/transfer.js
@@ -20,20 +20,22 @@ module.exports = {
 	async execute(interaction) {
         const member = interaction.member;
         const guild = interaction.guild;
+        const numFormat = (value) => new Intl.NumberFormat('en-US').format(value === null ? 0 : value);
         const recipient = interaction.options.getUser('player');
         const amount = interaction.options.getInteger('amount');
 
         console.log(recipient);
 
-        const player1 = await Player.findOne({ where: { discordID: member.id, guildID: guild.id }});
+        const player1 = await Player.findOne({ where: { discordID: member.id, guildID: guild.id }, include: 'iura'});
         const player2 = await Player.findOne({ where: { discordID: recipient.id, guildID: guild.id }});
 
-        if (!player1 || !player2) return interaction.reply({content: "This user does not have a player profile in this world yet.", ephemeral: true });
-        if (amount > player1.iura.walletAmount) return interaction.reply(`You do not have sufficient balance!`);
+        if (!player1 || !player2) return interaction.reply({ content: "This user does not have a player profile in this world yet.", ephemeral: true });
+        if (member.id === recipient.id) return interaction.reply({ content: "You can't transfer money to yourself!", ephemeral: true });
+        if (amount > player1.iura.walletAmount) return interaction.reply({ content: `You do not have sufficient balance!`, ephemeral: true });
 
         await player1.withdraw(-amount);
         await player2.withdraw(amount);
 
-        await interaction.reply(`\`${itemName}\` has been purchased.`);
+        await interaction.reply(`\`${numFormat(amount)} IURA\` has been transferred to ${player2.playerName}.`);
     }
 };
