c6ef4629a89ede08ed21c04f34a086abd0635a17
Kyanari <kyanari111@gmail.com>
2023-03-24 13:47:51 -0700

    Updated chatGPT

--- events/MessageCreate.js
+++ events/MessageCreate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, AttachmentBuilder } = require('discord.js');
 const { openAIkey, openAIorg } = require('../config.json');
 
 // Preparing connection to OpenAI API -----------------
@@ -12,25 +12,59 @@ const openai = new OpenAIApi(configuration);
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
+        if (message.author.bot) return;
+
 		try {
-
-            if (message.author.bot) return;
-            if (!message.content.startsWith('Dahlia')) return;
-    
-            const gptResponse = await openai.createCompletion({
-                model: 'text-davinci-003',
-                prompt: `Dahlia is an AI from the future who loves to quote famous authors and people. She was created by Cerberon to open and close portals around Eldelvain that would connect to different time periods from medieval, modern and future. She is smart, reserved, and capable AI who can be sarcastic and with sense of humor. She likes reading books and writing stories about Eldelvain. She can come up with entertaining stories that are engaging, imaginative and captivating for the audience. It can be fairy tales, educational stories or any other type of stories which has the potential to capture people's attention and imagination. She meets different adventurers fighting creatures called "Conflicts" which were created by Messinia Graciene. She doesn't know what Messinia Graciene is. She holds inventories for adventurers to store their items and equipment for battle. Her favorite food is ramen and her favorite author is Mark Twain. She dislikes watching television but is knowledgeable about TV shows. She lives in Eldelvain and likes to adopt cats. She doesn't want anyone know how old she is or her vital statistics.\n\
-                Dahlia: Hello, how are you doing?\n\
-                ${message.author.username}: ${message.content}\n\
-                Dahlia:`,
-                max_tokens: 100,
-                temperature: 0.9,
-                stop: ["Dahlia:", `${message.author.username}:`],
-            });
-
-            
             if (message.content.startsWith('Dahlia')) {
-                message.reply(`${gptResponse.data.choices[0].text}`);
+                let chatLog = [
+                    {
+                        role: 'system',
+                        content: `Your name is Dahlia and you're a battle AI from the future who loves to quote famous authors and people. You were created by Cerberon to open and close portals that would connect to different time periods. You respect Margaretha very much. You are smart, reserved, and capable AI. You like reading books and writing stories about Eldelvain where You lives. You can come up with entertaining stories that are engaging, imaginative and captivating for the audience. It can be fairy tales, educational stories or any other type of stories which has the potential to capture people's attention and imagination. You also store items and equipment for voyagers. Your favorite food is ramen and your favorite author is Mark Twain. You don't want anyone know how old you are.`
+                    },
+                    {
+                        role: 'assistant',
+                        content: 'Hello, how are you doing?'
+                    }
+                ];
+    
+                await message.channel.sendTyping();
+    
+                let prevChat = await message.channel.messages.fetch({ limit: 5 });
+                prevChat.reverse();
+                
+                prevChat.forEach((msg) => {
+                    if (msg.author.id === message.client.user.id && msg.author.bot) {
+                        chatLog.push({
+                            role: 'assistant',
+                            content: msg.content,
+                        });
+                    }
+    
+                    if (msg.author.id === message.author.id) {
+                        if (!msg.content.startsWith('Dahlia')) return;
+                        chatLog.push({
+                            role: 'user',
+                            content: msg.content,
+                        });
+                    }
+                });
+    
+                const result = await openai.createChatCompletion({
+                    model: 'gpt-3.5-turbo',
+                    max_tokens: 300,
+                    temperature: 0.9,
+                    messages: chatLog,
+                });
+    
+                const response = result.data.choices[0].message.content;
+                // console.log("Total tokens: ", result.data.usage.total_tokens);
+    
+                if (response.length >= 2000) {
+                    const attachment = new AttachmentBuilder(Buffer.from(response, 'utf-8'), { name: 'fromdahliatoyou.txt' });
+                    await message.reply({ content: `I couldn't give my whole answer here so I'm attaching the file for you.`, files: [attachment] });
+                } else {
+                    message.reply(`${response}`);
+                }
             }
         } catch(error) {
             if (error.APIerror) {
@@ -42,7 +76,7 @@ module.exports = {
                 message.reply(`Sorry, I'm getting a lot of requests right now. Please try again later.`);
             }
             else {
-                message.reply(`What are we talking about?`);
+                message.reply(`Yes?`);
                 console.log(`${error.response.status}: ${error.response.statusText}`);
             }
         }
